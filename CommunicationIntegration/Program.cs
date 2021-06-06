3884cb9f95b1f9b6a8111171fd51c5b8dc3dfd9f
WK WK <witold.kepczynski@gmail.com>
2021-06-06 23:28:53 +0200

    grpc communication is ok

---
+++ CommunicationIntegration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Grpc.Core;
+using Communication;
+
+namespace CommunicationIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("yello");
+            Channel channel = new Channel("127.0.0.1:5001", ChannelCredentials.Insecure);
+
+            var client = new PingPong.PingPongClient(channel);
+            var pong = client.PingPong(new Ping { Message = "Ping" });
+            System.Console.WriteLine($"pong:{pong}");
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
